b8fe9a6657f74271f1a7df4eb6dc7c680395c84e
杨 树. 宝 <yangshubao@hualala.com>
2018-04-17 14:34:49 +0800

    This version adds the xcode project

---
+++ TestCocoaPod/TestCocoaPod/Test.h
@@ -0,0 +1,14 @@
+//
+//  Test.h
+//  TestCocoaPod
+//
+//  Created by apple on 2018/4/17.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+- (void)log;
+@end
