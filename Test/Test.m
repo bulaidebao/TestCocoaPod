db6f2a0f4d621409f8c71f94cd4df4dcfac761ff
杨 树. 宝 <yangshubao@hualala.com>
2018-04-17 15:10:01 +0800

    This version adds the share class of test

---
+++ Test/Test.m
@@ -0,0 +1,18 @@
+//
+//  Test.m
+//  TestCocoaPod
+//
+//  Created by apple on 2018/4/17.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+- (void)log
+{
+    NSLog(@"你好");
+}
+
+@end
